03402afd1ebadfa15143710a40711f92d0bf0796
Joe Allen <allenjosephs@johndeere.com>
2020-01-27 14:30:06 -0600

    completed seed.js

--- src/db/seed.js
+++ src/db/seed.js
@@ -1,41 +1,47 @@
-const mongoose = require('./connection')
+// const mongoose = require('./connection')
 const Author = require('../models/Author')
 const Book = require('../models/Book')
 
 const seedData = require('./seedData')
 
-Author.deleteMany({}).then(() => console.log('deleted all authors'))
-Book.deleteMany({}).then(() => console.log('deleted all books'))
+Author.deleteMany({}).then(() => {
+  console.log('deleted all authors')
 
-seedData.forEach(book => {
-  //If the author exists, only add the book
-  Author.findOne({
-    name: book.author
-  }).then(author => {
-    Book.create({
-      title: book.title,
-      author: author.id,
-      description: book.description,
-      coverImgURL: book.coverImgURL
-    }).then(newBook => {
-      author.books.push(newBook)
-      author.save()
-      console.log(`created ${newBook.title}`)
-    })
-  }).catch(err => {
-    //author not found, add both
-      Author.create({
+  Book.deleteMany({}).then(() => {
+    console.log('deleted all books')
+
+    seedData.forEach(book => {
+      //If the author exists, only add the book
+      Author.findOne({
         name: book.author
       }).then(author => {
         Book.create({
-        title: book.title,
-        author: author.id,
-        description: book.description,
-        coverImgURL: book.coverImgURL
-      }).then(newBook => {
-        author.books.push(newBook)
-        author.save()
-        console.log(`created ${newBook.title}`)
+          title: book.title,
+          author: author.id,
+          description: book.description,
+          coverImgURL: book.coverImgURL
+        }).then(newBook => {
+          author.books.push(newBook)
+          author.save()
+          console.log(`created ${newBook.title}`)
+        })
+      }).catch(err => {
+        //author not found, add both
+          Author.create({
+            name: book.author
+          }).then(author => {
+            Book.create({
+            title: book.title,
+            author: author.id,
+            description: book.description,
+            coverImgURL: book.coverImgURL
+          }).then(newBook => {
+            author.books.push(newBook)
+            author.save()
+            console.log(`created author: ${author.name}`)
+            console.log(`created book: ${newBook.title}`)
+          })
+        })
       })
     })
   })
