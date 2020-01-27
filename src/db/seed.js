614e41a859e7ffacda8dd2df5bc827eacedad4c8
Joe Allen <allenjosephs@johndeere.com>
2020-01-27 14:55:09 -0600

    seed for basic books and authors

--- src/db/seed.js
+++ src/db/seed.js
@@ -8,44 +8,21 @@ Author.deleteMany({}).then(() => {
 
   Book.deleteMany({}).then(() => {
     console.log('deleted all books')
-
     for (const book of seedData) {
-      //If the author exists, only add the book
-      Author.findOne({
+      Author.create({
         name: book.author
       }).then(author => {
-
-        if (author) {
-          console.log("found author: ", author)
-          Book.create({
-            title: book.title,
-            author: author.id,
-            description: book.description,
-            coverImgURL: book.coverImgURL
-          }).then(newBook => {
-            author.books.push(newBook)
-            author.save()
-            console.log(`created ${newBook.title}`)
-          })
-        } else {
-          //author not found, add both
-          console.log(`author ${book.author} not found`)
-          Author.create({
-            name: book.author
-          }).then(author => {
-            Book.create({
-            title: book.title,
-            author: author.id,
-            description: book.description,
-            coverImgURL: book.coverImgURL
-            }).then(newBook => {
-              author.books.push(newBook)
-              author.save()
-              console.log(`created author: ${author.name}`)
-              console.log(`created book: ${newBook.title}`)
-            })
-          })
-        }
+        Book.create({
+        title: book.title,
+        author: author.id,
+        description: book.description,
+        coverImgURL: book.coverImgURL
+        }).then(newBook => {
+          author.books.push(newBook)
+          author.save()
+          console.log(`created author: ${author.name}`)
+          console.log(`created book: ${newBook.title}`)
+        })
       })
     }
   })
