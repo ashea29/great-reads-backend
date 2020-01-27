0f7b8edc38c76755ec1ea1742208736e7f5725de
Joe Allen <allenjosephs@johndeere.com>
2020-01-27 17:24:32 -0600

    completed authors controller

--- src/controllers/authors.js
+++ src/controllers/authors.js
@@ -3,10 +3,48 @@ const router = express.Router()
 
 const Author = require('../models/Author')
 
+// Get all authors (incl. book details)
 router.get("/", (req, res) => {
-  Author.find({}).then(allAuthors => {
-    res.json(allAuthors)
+  Author.find({})
+		.populate('books', [
+			'_id',
+			'title',
+			'description',
+			'coverImgUrl'
+		])
+		.then(allAuthors => {
+			res.json(allAuthors)
+		})
+})
+
+// Get author by ID (incl. book details)
+router.get("/:id", (req, res) => {
+  Author.findOne({ _id: req.params.id })
+		.populate('books', [
+			'_id',
+			'title',
+			'description',
+			'coverImgUrl'
+		])
+		.then(author => res.json(author))
+})
+
+// Add new author
+router.post('/', (req, res) => {
+  Author.create(req.body)
+    .then(author => {
+      res.redirect('/authors')
   })
+})
+
+// Update existing author
+router.put('/:id', (req, res) => {
+  console.log(req.body)
+  Author.findOneAndUpdate({_id: req.params.id},
+    req.body, {new: true})
+    .then(author => {
+      res.redirect(303, '/authors')
+    })
 })
 
 module.exports = router;
