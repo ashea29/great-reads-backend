08687bf0d3579b4f4ec0400272012b7a1b86cedf
Joe Allen <allenjosephs@johndeere.com>
2020-01-28 13:30:43 -0600

    corrected author by name route and added new no-populate routes

--- src/controllers/authors.js
+++ src/controllers/authors.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const Author = require('../models/Author')
 
+/////////////// ROUTES TO GET AUTHOR DATA POPULATED WITH BOOK DETAILS  /////////////////
 // Get all authors (incl. book details)
 router.get("/", (req, res) => {
   Author.find({})
@@ -29,20 +30,9 @@ router.get("/:id", (req, res) => {
 		.then(author => res.json(author))
 })
 
-// Get author by ID (with no book details)
-router.get("/noDetails/:id", (req, res) => {
-  Author.findOne({ _id: req.params.id })
-		.then(author => res.json(author))
-})
-
-// Get all authors (with no book details)
-router.get("/noDetails/", (req, res) => {
-  Author.find({}).then(allAuthors => res.json(allAuthors))
-})
-
 // Get author by Name (incl. book details)
 router.get("/byName/:authName", (req, res) => {
-  Author.findOne({ _id: req.params.authName })
+  Author.findOne({ name: req.params.authName })
 		.populate('books', [
 			'_id',
 			'title',
@@ -51,6 +41,29 @@ router.get("/byName/:authName", (req, res) => {
 		])
 		.then(author => res.json(author))
 })
+//////////////// END GET-POPULATE ROUTES //////////////////////////////////
+
+
+/////////////// ROUTES TO GET AUTHOR DATA WITHOUT POPULATE  /////////////////
+// Get all authors (with no book details)
+router.get("/noDetails/all", (req, res) => {
+  Author.find({}).then(allAuthors => res.json(allAuthors))
+})
+
+// Get author by ID (with no book details)
+router.get("/noDetails/:id", (req, res) => {
+	console.log('here2')
+   Author.findOne({ _id: req.params.id })
+	 	.then(author => res.json(author))
+})
+
+// Get author by Name (with no book details)
+router.get("/noDetails/byName/:authName", (req, res) => {
+  Author.findOne({ name: req.params.authName })
+		.then(author => res.json(author))
+})
+//////////////// END GET-NO-POPULATE ROUTES //////////////////////////////////
+
 
 // Add new author, return all authors in the DB
 router.post('/', (req, res) => {
