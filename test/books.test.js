5ab6c045c36a145840920db973aae46e729f0bdb
ashea27 <shea.andrew27@gmail.com>
2020-01-30 10:20:35 -0700

    wrote test for DELETE route

--- test/books.test.js
+++ test/books.test.js
@@ -193,51 +193,88 @@ describe('PUT /books/:id', () => {
 })
 
 
-// describe('DELETE /books/:id', () => {
+describe('DELETE /books/:id', () => {
 
-//     let booksPrevLength
-//     let authBooksPrevLength
-//     let bookToDelete
-  
-//     before((done) => {
-//       api.post('/books/')
-//           .set('Accept', 'application/json')
-//           .send({
-//             'id': booksPrevLength + 1,
-//             'title': 'Test',
-//             'author': {name: 'Tester'},
-//             'description': 'Testing...1..2..3..',
-//             'coverImgURL': 'http://awesomeURL'
-//           })
-//           done()
-//     })
+    let booksPrevLength
+    let authBooksPrevLength
+    let id
+    let authorID
+    let authName
 
-//     before(done => {
-//         api
-//             .get('/')
-//             .set('Accept', 'application/json')
-//             .end((error, response) => {
-//                 booksPrevLength = response.body.length
-//                 bookToDelete = response.body[response.body.length - 1]._id
-//                 done()
-//             })
-//     })
-  
-//     before(done => {
-//         api.delete(`/blogposts/${blogToDelete}`)
-//             .set('Accept', 'application/json')
-//             .end((error, response) => {
-//                 done()
-//             })
-//     })
-  
-//     it('deletes a blog by id', done => {
-//         api.get('/blogposts')
-//             .set('Accept', 'application/json')
-//             .end((error, response) => {
-//                 expect(response.body.length).to.equal(previousLength - 1)
-//                 expect(response.body.find((blog) => blog.id == blogToDelete)).to.equal(undefined)
-//                 done()
-//             })
-//     })
-//   })
+    before(done => {
+      api.get('/')
+          .set('Accept', 'application/json')
+          .end((error, response) => {
+            booksPrevLength = response.body.length
+            done()
+          })
+    })
+
+  before((done) => {
+    api.get('/authors/random/random')
+        .set('Accept', 'application/json')
+        .end((error, response) => {
+          authName = response.body.name
+          authorID = response.body._id
+          done()
+        })
+  })
+
+  before(done => {
+    api.get(`/authors/${authorID}`)
+        .set('Accept', 'application/json')
+        .end((error, response) => {
+          authBooksPrevLength = response.body.books.length
+          done()
+        })
+  })
+
+  before((done) => {
+    console.log(authName)
+    api.post(`/books/${authName}`)
+        .set('Accept', 'application/json')
+        .send({
+          title: 'Test10',
+          description: 'Testing...1..2..3..',
+          coverImgURL: 'http://awesomeURL'
+        })
+        .end((error, response) => {
+          console.log(`added book with title: ${response.body.title}`)
+          id = response.body._id
+          done()
+        })
+  })
+
+
+  before(done => {
+    console.log(id)
+    api.del(`/books/${id}`)
+        .set('Accept', 'application/json')
+        .end((error, response) => {
+          done()
+        })
+  })
+
+  it('deletes a book by id from the books collection', done => {
+    api.get('/')
+        .set('Accept', 'application/json')
+        .end((error, response) => {
+          // console.log(response.body)
+            expect(response.body.length).to.equal(booksPrevLength)
+            expect(response.body.find((book) => book._id == id)).to.equal(undefined)
+            done()
+        })
+  })
+
+  it('deletes a book by id from the books array of its author', done => {
+    api.get(`/authors/${authorID}`)
+        .set('Accept', 'application/json')
+        .end((error, response) => {
+            expect(response.body.books.length).to.equal(authBooksPrevLength)
+            expect(response.body.books.find((book) => book._id == id)).to.equal(undefined)
+            done()
+        })
+  })
+
+})
+
